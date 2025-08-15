fcaa9e2e1254b78316f9a1bf1fb61f54bf6cfb36
gundlachj <gundlachj@msoe.edu>
2025-08-15 01:24:59 -0400

    added support for bat instead of less

--- tmux-cht.sh
+++ tmux-cht.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
+
+print_cmd=less
+if hash bat 2>/dev/null; then
+  print_cmd=bat
+fi
+
 selected=`cat ~/.tmux/.tmux-cht-languages ~/.tmux/.tmux-cht-command | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
+echo $print_cmd
 read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl cht.sh/$selected/$query | $print_cmd"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | $print_cmd"
 fi
 
